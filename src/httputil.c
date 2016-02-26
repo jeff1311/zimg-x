9d76646bb119aa29396c446b41f379b901b80297
liangjixun <120295177@qq.com>
2016-02-26 09:16:51 +0800

    test

--- src/httputil.c
+++ src/httputil.c
@@ -37,7 +37,7 @@ int sendhttp()
         bzero(&servaddr, sizeof(servaddr));
         servaddr.sin_family = AF_INET;
         servaddr.sin_port = htons(PORT);
-        servaddr.sin_addr = IPSTR;
+        servaddr.sin_addr.s_addr = inet_addr(IPSTR);
 		printf("servaddr.sin_addr = %s",servaddr.sin_addr);
         if (inet_pton(AF_INET, IPSTR, &servaddr.sin_addr) <= 0 ){
                 printf("创建网络连接失败,本线程即将终止--inet_pton error!\n");
